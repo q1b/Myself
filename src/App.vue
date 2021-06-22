001e511ecb85fca4c133af51406a55b8f4fd487e
sacarvy <sukhpreetben10@gmail.com>
2021-06-22 17:59:10 +0530

    Instial COmmit V4

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex ">
-  <header class="sticky hidden md:flex top-0 left-0 z-0 w-0 h-screen">
+  <header class="md:sticky hidden md:flex top-0 left-0 z-0 w-0 h-screen">
     <main-header :right="false" ></main-header>
   </header>
     <router-view />
-  <header class="sticky md:hidden top-0 left-0 z-0 w-0 h-screen">
+  <header class="md:sticky md:hidden top-0 left-0 z-0 w-0 h-screen">
     <main-header :right="true" ></main-header>
   </header>
   </div>
